65bf8d0b8c1cea3af2d1f80b06caeb72a577007b
JESSURI CRISTAL BAEZA CRUZ <cristal200479@gmail.com>
2025-11-10 14:26:50 -0700

    ya esta la creacion de padres y terapeutas

--- src/app/terapeutas/pages/inicio/inicio.ts
+++ src/app/terapeutas/pages/inicio/inicio.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   imports: [],
+<<<<<<< HEAD
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss',
+=======
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.scss',
+>>>>>>> 65a66f7 (ya esta la creacion de padres y terapeutas)
 })
 export class Inicio {
 
